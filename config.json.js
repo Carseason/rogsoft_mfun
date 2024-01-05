8ce475231d6de6763502f501ac5cf7ab2473b13b
Carseason <757841203@qq.com>
2024-01-05 22:13:35 +0800

    0.0.9

--- config.json.js
+++ config.json.js
@@ -1,8 +1,8 @@
 {
     "description": "轻量影视媒体库", 
     "home_url": "Module_mfun.asp", 
-    "md5": "7f76284e8dea3779aad7a14b09498d79", 
+    "md5": "70e8518895a5db746efd107ab13d8b86", 
     "module": "mfun", 
     "title": "MFUN", 
-    "version": "0.0.8"
+    "version": "0.0.9"
 }
